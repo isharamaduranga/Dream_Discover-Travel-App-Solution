5b3e9f3f597633a42c4fe56e8fdfe328c3182f9d
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-01-27 00:37:49 +0530

    content updated ...

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,6 +1,6 @@
-
+/* http://13.201.59.0/api/v1/placesWithComments */
 const apiConfig = {
-    serverUrl: 'http://127.0.0.1:8000',
+    serverUrl: 'http://13.201.59.0',
     basePath: 'api/v1',
     port: 80
 }
